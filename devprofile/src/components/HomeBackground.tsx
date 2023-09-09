38365dd77f29e39f3408e04c5a3e489147dafc5e
Sou <samelsonjohn@protonmail.com>
2023-09-09 16:42:35 -0400

    Adding home background SVG

---
+++ devprofile/src/components/HomeBackground.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeBackground: React.FC = () => {
+  return (
+    <div className="w-full h-full">
+      <img src="/SouBackground.svg" alt="Background Image" className="w-full h-full object-cover" />
+    </div>
+  );
+}
+
+export default HomeBackground;
--- devprofile/src/pages/index.tsx
+++ devprofile/src/pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import HomeBackground from '../components/HomeBackground';
+
 
 const Home: React.FC = () => {
   return (
     <div>
       <NavBar />
+      <HomeBackground />
     </div>
   );
 }
