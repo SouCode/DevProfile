47dcc0e95a1aa7f8a1af579c546278c781bb9936
Sou <samelsonjohn@protonmail.com>
2023-09-09 16:35:11 -0400

    Nav semi complete

--- devprofile/src/components/NavBar.tsx
+++ devprofile/src/components/NavBar.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link';
 
 const NavBar: React.FC = () => {
   return (
-    <div className="relative w-full h-52 bg-white flex items-center">
-      <img src="/SouCodeLogo.svg" alt="SouCode Logo" className="absolute" style={{ top: '14px', left: '82px' }} />
-      <div className="ml-auto space-x-4">
-        <Link href="#About"><a className="text-gray-700 hover:text-black" style={{ top: '20px', left: '935px' }}>About</a></Link>
-        <Link href="#Work"><a className="text-gray-700 hover:text-black" style={{ top: '20px', left: '1073px' }}>Work</a></Link>
-        <Link href="#Contact"><a className="text-gray-700 hover:text-black" style={{ top: '20px', left: '1165px' }}>Contact</a></Link>
+    <div className="flex justify-between items-center bg-white p-4" style={{ width: '1153px', height: '100px' }}>
+      <img src="/SouCodeLogo.svg" alt="SouCode Logo" />
+      <div className="space-x-4">
+        <Link href="#About" className="text-gray-700 hover:text-black">#About</Link>
+        <Link href="#Work" className="text-gray-700 hover:text-black">#Work</Link>
+        <Link href="#Contact" className="text-gray-700 hover:text-black">#Contact</Link>
       </div>
     </div>
   );
