f15abc5cbc5724f36b49195caa49abff9beb088e
Sou <samelsonjohn@protonmail.com>
2023-09-09 23:52:51 -0400

    fixed Link error

--- devprofile/src/components/NavBar.tsx
+++ devprofile/src/components/NavBar.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const NavBar: React.FC = () => {
+  const router = useRouter();
+
+  const navigateTo = (section: string) => {
+    void router.push(section);
+  };
+
   return (
     <div className="flex justify-between items-center w-full bg-white p-4">
       <img src="/SouCodeLogo.svg" alt="SouCode Logo" />
       <div className="space-x-4" style={{ width: '827px', justifyContent: 'flex-end', display: 'flex' }}>
-        <Link href="#About" className="text-gray-700 hover:text-black">About</Link>
-        <Link href="#Work" className="text-gray-700 hover:text-black">Work</Link>
-        <Link href="#Contact" className="text-gray-700 hover:text-black">Contact</Link>
+        <button onClick={() => navigateTo("#About")} className="text-gray-700 hover:text-black">
+          About
+        </button>
+        <button onClick={() => navigateTo("#Work")} className="text-gray-700 hover:text-black">
+          Work
+        </button>
+        <button onClick={() => navigateTo("#Contact")} className="text-gray-700 hover:text-black">
+          Contact
+        </button>
       </div>
     </div>
   );
