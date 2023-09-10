ee8aab459f9c507e6b366526bba95a347fdc6f56
Sou <samelsonjohn@protonmail.com>
2023-09-10 17:36:21 -0400

    SouPic Frame added

--- devprofile/src/components/About.tsx
+++ devprofile/src/components/About.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 const About: React.FC = () => {
   return (
-    <div className="h-screen bg-white">
-      {/* Your About section content goes here */}
-      <h1>About Me</h1>
-      <p>This is the about section...</p>
+    <div className="h-screen bg-white relative">
+      <div 
+        className=" absolute top-5 left-13 w-[538.5px] h-[304.11px] flex items-center justify-center"
+      >
+        <img src="/SouPicFrame.svg" alt="Sou Pic Frame" className="w-[90%] h-[90%]" />
+      </div>
+      {/* Other content for the About section can go here */}
     </div>
   );
 }
