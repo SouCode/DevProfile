63947a315dd2eab297b0134a613f6d1dc320be76
Sou <samelsonjohn@protonmail.com>
2023-09-10 22:10:33 -0400

    About Me box created

--- devprofile/src/components/About.tsx
+++ devprofile/src/components/About.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 
 const About: React.FC = () => {
-  return (
-    <div className="h-screen bg-white relative">
-      <div 
-        className=" absolute top-5 left-13 w-[538.5px] h-[304.11px] flex items-center justify-center"
-      >
-        <img src="/SouPicFrame.svg" alt="Sou Pic Frame" className="w-[90%] h-[90%]" />
-      </div>
-      {/* Other content for the About section can go here */}
-    </div>
-  );
+    return (
+        <div className="h-screen bg-white relative">
+            <div
+                className="absolute top-5 left-13 w-[538.5px] h-[304.11px] flex items-center justify-center"
+            >
+                <img src="/SouPicFrame.svg" alt="Sou Pic Frame" className="w-[90%] h-[90%]" />
+            </div>
+            {/* About Me Box */}
+            <div
+                className="border-black absolute flex items-center justify-center p-4"
+                style={{
+                    top: '35px',
+                    left: '560.5px',
+                    width: '846px',
+                    height: '273px',
+                    borderTopWidth: '4px',
+                    borderLeftWidth: '4px',
+                    borderBottomWidth: '4px',
+                    borderRightWidth: '4px',
+                }}
+            >
+                <p>Your paragraph content here...</p>
+            </div>
+
+        </div>
+    );
 }
 
 export default About;
