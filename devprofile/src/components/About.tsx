c0bf83b5e7cb2b6d7ed3570663309bbacbb559b9
Sou <samelsonjohn@protonmail.com>
2023-09-17 22:55:44 -0400

    Making Contriubution into Grid

--- devprofile/src/components/About.tsx
+++ devprofile/src/components/About.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import '../styles/contributions.css';
+
 
 interface ContributionData {
   date: string;
